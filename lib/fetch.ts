6540a7b1cd2cb5c6bc8f6b5a33cc761012bb6a9b
Huu Phuong <phuonghuu71@gmail.com>
2024-10-10 22:28:05 +0700

    Change fetch method

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -3,7 +3,7 @@ import { Vehicle } from "./column";
 
 export async function fetchData(token: string): Promise<Vehicle[]> {
     const response = await axios.get(
-        "https://dev254205.service-now.com/api/x_1281635_vehicles/v2/vehicles/vehicles?name=Nissan",
+        "https://dev254205.service-now.com/api/x_1281635_vehicles/v2/vehicles/vehicles",
         {
             headers: {
                 Authorization: `Bearer ${token}`, // Use accessToken in Authorization header
